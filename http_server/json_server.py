062df0df4c9292abf7f18a22e88c1e6fe8ba2f3d
mrf1n <mrf1n.dev@gmail.com>
2025-02-08 14:27:56 +0100

    api cors

--- http_server/json_server.py
+++ http_server/json_server.py
@@ -10,6 +10,11 @@ class SimpleJSONHandler(BaseHTTPRequestHandler):
             
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
+
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            self.send_header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
             self.end_headers()
             
             self.wfile.write(json.dumps(data).encode('utf-8'))
