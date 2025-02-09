9d2a1166b8a5ed3b25f3d2dd59f98a4f31fa689b
mrf1n <mrf1n.dev@gmail.com>
2025-02-09 01:04:59 +0100

    Policy Builder

--- src/pages/PolicyBuilder.tsx
+++ src/pages/PolicyBuilder.tsx
@@ -1,79 +1,148 @@
-import {useState} from "react";
-import {Button} from "@/components/ui/button";
-import {addPolicy} from "@/localStorageService";
-import {Policy} from "@/types";
-import axios from "axios";
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { DatePicker } from "@/components/blocks/date-picker";
+import { Link } from "react-router";
+
+const insuranceOptions = [
+    { id: 1, label: 'Pojazd', image: 'https://img.icons8.com/?size=100&id=12666&format=png' },
+    { id: 2, label: 'Mieszkanie', image: 'https://img.icons8.com/?size=100&id=73&format=png' },
+    { id: 3, label: 'Podróż', image: 'https://img.icons8.com/?size=100&id=648&format=png' },
+    { id: 4, label: 'Zdrowie życie', image: 'https://img.icons8.com/?size=100&id=35583&format=png' },
+    { id: 5, label: 'Biznes', image: 'https://img.icons8.com/?size=100&id=7Yl8hX5gHFhC&format=png' },
+    { id: 6, label: 'Rolne', image: 'https://img.icons8.com/?size=100&id=794&format=png' }
+];
 
 export const PolicyBuilder = () => {
-    const [data, setData] = useState<Policy>();
-    const handleClickNewPolicy = () => {
-        axios.get("http://localhost:8000")
-            .then(response => {
-                const policy = randomizeData(mapData(response.data));
-                // @ts-expect-error
-                setData(policy)
-                // @ts-expect-error
-                addPolicy(policy)
-            });
-    }
+    const [startDate, setStartDate] = useState();
+    const [postCode, setPostCode] = useState('');
+    const [postCodeError, setPostCodeError] = useState('');
+    const [selectedOptions, setSelectedOptions] = useState([]);
+
+    const [showFields, setShowFields] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const handleOptionChange = (id) => {
+        setSelectedOptions((prev) =>
+            prev.includes(id) ? prev.filter((option) => option !== id) : [...prev, id]
+        );
+    };
+
+    const handleCompare = () => {
+        setShowFields(true);
+    };
 
     return (
-        <div>
-            <Button onClick={handleClickNewPolicy}>Nowy polis</Button>
-            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-        </div>
+        <div className="p-6 max-w-xl mx-auto space-y-4">
+            <h1 className="text-2xl font-bold text-center">Znajdź najlepszą ofertę</h1>
+            {!showFields && (
+                <div className="flex items-center space-x-4 p-4 justify-between">
+                    <label htmlFor="start-date" className="font-medium text-gray-800 w-50 md:w-auto">
+                        Od kiedy potrzebujesz ubezpieczenia?
+                    </label>
+                    <DatePicker
+                        date={startDate || new Date()}
+                        setDate={(data) => { setStartDate(data) }}
+                    />
+                </div>
+            )}
+            {!showFields && (
+                <Input
+                    type="text"
+                    value={postCode}
+                    onChange={(e) => {
+                        const value = e.target.value;
+                        setPostCode(value);
+                        const postalCodePattern = /^[0-9]{2}-[0-9]{3}$/;
+
+                        if (!postalCodePattern.test(value)) {
+                            setPostCodeError('Please enter a valid postal code (e.g., 12-345)');
+                        } else {
+                            setPostCodeError('');
+                        }
+                    }}
+                    placeholder="Podaj kod pocztowy"
+                />
+            )}
+            {!showFields && postCodeError && <span className="text-red-500 text-sm">{postCodeError}</span>}
+
+            {!showFields && (
+                <div className="grid grid-cols-3 gap-4">
+                    {insuranceOptions.map((option) => (
+                        <div
+                            key={option.id}
+                            className={`cursor-pointer p-4 text-center rounded-2xl shadow-md border-2 ${selectedOptions.includes(option.id) ? 'border-blue-500' : 'border-gray-300'
+                                }`}
+                            onClick={() => handleOptionChange(option.id)}>
+                            <picture>
+                                <img src={`${option.image}&color=FF0000}`} alt={option.label} className="mx-auto mb-2" />
+                            </picture>
+                            <div>{option.label}</div>
+                        </div>
+                    ))}
+                </div>
+            )}
+            <div className="p-4">
+                {showFields && (
+                    <div className="mt-4">
+                        <div className="mb-4">
+                            <label htmlFor="name" className="block font-medium text-gray-800">
+                                Name
+                            </label>
+                            <input
+                                id="name"
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                placeholder="Enter your name"
+                                className="w-full border p-2 rounded"
+                            />
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="email" className="block font-medium text-gray-800">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="Enter your email"
+                                className="w-full border p-2 rounded"
+                            />
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="phone" className="block font-medium text-gray-800">
+                                Phone Number
+                            </label>
+                            <input
+                                id="phone"
+                                type="tel"
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                                placeholder="Enter your phone number"
+                                className="w-full border p-2 rounded"
+                            />
+                        </div>
+                        <Button asChild className="w-full bg-blue-500 text-white py-2 rounded-2xl">
+                            <Link to={`/polisy`}>
+                                Wyślij do agenta
+                            </Link>
+                        </Button>
+                    </div>
+                )}
+
+                {!showFields && (<Button className="w-full bg-blue-500 text-white py-2 rounded-2xl" onClick={handleCompare}>
+                    Porównaj oferty
+                </Button>)}
+
+
+
+            </div>
+        </div >
     );
-}
-
-
-const mapData = (apiData: any) => {
-    return {
-        policyId: apiData.policyId,
-        insurerBrandName: apiData.insurerBrandName,
-        insurancePeriodValidFromDate: apiData.insurancePeriodValidFromDate,
-        insurancePeriodValidToDate: apiData.insurancePeriodValidToDate,
-        documentList: apiData.documentList.map((document: any) => ({
-            documentId: document.documentId,
-            type: document.type,
-            fileName: document.fileName,
-            description: document.description,
-        })),
-        premiumInstallmentList: apiData.premiumInstallmentList.map((premiumInstallment: any) => ({
-            installmentId: premiumInstallment.installmentId,
-            policyId: premiumInstallment.policyId,
-            value: premiumInstallment.value,
-            currency: premiumInstallment.currency,
-            installmentNo: premiumInstallment.installmentNo,
-            dueDate: premiumInstallment.dueDate
-        }))
-    }
-}
-
-const randomizeData = (apiData: any) => {
-    return {
-        policyId: crypto.randomUUID(),
-        insurerBrandName: apiData.insurerBrandName + "_" + crypto.randomUUID().slice(0, 8),
-        insurancePeriodValidFromDate: new Date(
-            Date.parse(apiData.insurancePeriodValidFromDate) +
-            Math.floor(Math.random() * 365) * 24 * 60 * 60 * 1000
-        ).toISOString(),
-        insurancePeriodValidToDate: new Date(
-            Date.parse(apiData.insurancePeriodValidToDate) +
-            (Math.floor(Math.random() * 365 * 5) + 365 * 5) * 24 * 60 * 60 * 1000
-        ).toISOString(),
-        documentList: apiData.documentList.map((document: any) => ({
-            documentId: crypto.randomUUID(),
-            type: document.type,
-            fileName: document.fileName,
-            description: document.description,
-        })),
-        premiumInstallmentList: apiData.premiumInstallmentList.map((premiumInstallment: any) => ({
-            installmentId: premiumInstallment.installmentId,
-            policyId: premiumInstallment.policyId,
-            value: premiumInstallment.value,
-            currency: premiumInstallment.currency,
-            installmentNo: premiumInstallment.installmentNo,
-            dueDate: premiumInstallment.dueDate
-        }))
-    }
 }
